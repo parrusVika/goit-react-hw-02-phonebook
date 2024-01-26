d0f5b2f341b2b51023318e0817cf5e2966d0778c
Парасюк Виктория <107310178+parrusVika@users.noreply.github.com>
2024-01-26 09:59:08 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ import { FormContacts } from 'components/Contactsform/Contactsform';
 import { render } from 'react-dom';
 import { Component } from 'react';
 import { ContactList } from 'components/ContactsList/ContactsList';
-// const contactsData = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
 
 export class App extends Component {
   state = {
